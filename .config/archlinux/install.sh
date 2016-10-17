c5a9dfbcb44b5877178b7e44609ff08024e6932b
Nicolas Ries <n.ries@minuskel.de>
2016-10-17 13:51:48 +0000

    add archlinux package script

---
+++ .config/archlinux/install.sh
@@ -0,0 +1,11 @@
+if [ "$1" == "core" ]
+then
+    pacman -S - < core.txt
+elif [ "$1" == "full" ]
+then
+    pacman -S - < core.txt
+    pacman -S - < full.txt
+else
+    echo "specify \"core\" or \"full\""
+fi
+
