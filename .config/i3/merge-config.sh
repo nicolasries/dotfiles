ef8bce23600637d734ff916566350e8330db4159
Nicolas Ries <nicolas.ries@t-online.de>
2017-05-21 13:45:01 +0200

    remove local i3 config

--- .config/i3/merge-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -f ~/.config/i3/config.local ]
-    then
-        cat ~/.config/i3/config.base ~/.config/i3/config.local > ~/.config/i3/config
-    else
-        cat ~/.config/i3/config.base > ~/.config/i3/config
-fi
