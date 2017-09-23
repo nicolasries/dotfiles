4932b22bccd8064789a4cd4184244ad7f1a1b2e0
Nicolas Ries <nicolas.ries@t-online.de>
2017-09-23 18:28:53 +0200

    add volume script

---
+++ bin/volume.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+duration=200
+function volume_notify() {
+    notify-send -t $duration -u low -i /usr/share/icons/breeze-dark/actions/24/player-volume.svg "$(amixer get Master|tail -n1|sed -E 's/.*\[([0-9]+)\%\].*/\1/')"
+}
+
+function toggle_notify() {
+    notify-send -t $duration -u low -i /usr/share/icons/breeze-dark/actions/24/player-volume-muted.svg " "
+}
+
+case $1 in
+    "up")
+        pactl set-sink-volume 0 +5%
+        volume_notify
+        ;;
+    "down")
+        pactl set-sink-volume 0 -5%
+        volume_notify
+        ;;
+    "toggle")
+        pactl set-sink-mute 0 toggle
+        toggle_notify
+        ;;
+esac
