8c18ba9141e429c13daf4d9014100debf57a2a83
Nico Ries <nicolas.ries@t-online.de>
2018-11-25 22:28:41 +0100

    upd

--- .config/sway/start.sh
+++ .config/sway/start.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
-  # keyboard settings here for now, can be moved to sway config with 1.0 
-  export WLC_REPEAT_RATE=50
-  export WLC_REPEAT_DELAY=300
-  export XKB_DEFAULT_LAYOUT=us,de
-  export XKB_DEFAULT_OPTIONS="grp:win_space_toggle,caps:escape"
-
   exec sway
 fi
 
