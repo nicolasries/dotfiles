b1870d38234b78eeb9df041322adedf78ed4d1aa
Nico Ries <nicolas.ries@t-online.de>
2018-11-30 15:57:44 +0100

    upd

--- .config/i3/switch_workspace.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-i3-msg workspace "$(xrdb -query all | grep workspace."$1".name | awk '{print $2}')"
--- .config/sway/start.sh
+++ .config/sway/start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
-  exec sway
+    # enable Wayland on some tool kits
+    QT_QPA_PLATFORM=wayland-egl
+    QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+
+    exec sway
 fi
 
