bc5a503f39ab080cef2a9b9402b31fa74a6cc49a
Nico Ries <nicolas.ries@t-online.de>
2018-12-17 09:03:01 +0100

    cleanup

--- .config/sway/start.sh
+++ .config/sway/start.sh
@@ -2,8 +2,8 @@
 
 if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
     # enable Wayland on some tool kits
-    QT_QPA_PLATFORM=wayland-egl
-    QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+    export QT_QPA_PLATFORM=wayland-egl
+    export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
 
     exec sway
 fi
