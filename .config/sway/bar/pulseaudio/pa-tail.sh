28ac45d67fbe301d76566e59f497097bb9e83d96
Nico Ries <nicolas.ries@t-online.de>
2018-12-19 09:30:45 +0100

    upd:

---
+++ .config/sway/bar/pulseaudio/pa-tail.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+sink="@DEFAULT_SINK@"
+# sink=0
+
+volume_up() {
+    pactl set-sink-volume $sink +1%
+}
+
+volume_down() {
+    pactl set-sink-volume $sink -1%
+}
+
+volume_mute() {
+    pactl set-sink-mute $sink toggle
+}
+
+volume_print() {
+    if pacmd list-sinks | grep active | head -n 1 | grep -q speaker; then
+        icon="#1"
+    elif pacmd list-sinks | grep active | head -n 1 | grep -q headphones; then
+        icon="#2"
+    else
+        icon="#3"
+    fi
+
+    muted=$(pamixer --sink $sink --get-mute)
+
+    if [ "$muted" = true ]; then
+        echo "$icon --"
+    else
+        echo "$icon $(pamixer --sink $sink --get-volume)"
+    fi
+}
+
+listen() {
+    volume_print
+
+    pactl subscribe | while read -r event; do
+            volume_print
+    done
+}
+
+case "$1" in
+    --up)
+        volume_up
+        ;;
+    --down)
+        volume_down
+        ;;
+    --mute)
+        volume_mute
+        ;;
+    *)
+        listen
+        ;;
+esac
+
