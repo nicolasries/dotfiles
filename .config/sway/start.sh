781aad784985da9cc6b034dfd51bd85301cd78e2
Nico Ries <nicories@posteo.de>
2019-07-24 22:59:24 +0200

    fix gdk bugs

--- .config/sway/start.sh
+++ .config/sway/start.sh
@@ -4,7 +4,8 @@
     # export QT_QPA_PLATFORM=wayland-egl
     # export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
 
-    export GDK_BACKEND=wayland
+    # screws up thunderbird and emacs, disabled for now
+    # export GDK_BACKEND=wayland
 
     export MOZ_ENABLE_WAYLAND=1
     
