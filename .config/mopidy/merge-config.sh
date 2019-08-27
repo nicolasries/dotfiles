161dca15ce772c719b7e4e459a2fa898cacdfedd
Nico Ries <nicories@posteo.de>
2019-08-27 19:35:40 +0200

    upd

--- .config/mopidy/merge-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -f ~/.config/mopidy/spotify.conf ] 
-    then
-        cat ~/.config/mopidy/mopidy.conf.base ~/.config/mopidy/spotify.conf > ~/.config/mopidy/mopidy.conf
-    else
-        cat ~/.config/mopidy/mopidy.conf.base > ~/.config/mopidy/mopidy.conf
-fi
