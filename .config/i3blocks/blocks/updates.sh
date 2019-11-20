4b651b3483414ce0184e64c8d8ac86e52dcbd4e9
Nico Ries <nicories@posteo.de>
2019-11-20 09:31:31 +0100

    upd

--- .config/i3blocks/blocks/updates.sh
+++ .config/i3blocks/blocks/updates.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
 
-if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
-    updates_arch=0
-fi
+refresh() {
+    pkill -RTMIN+6 i3blocks
+}
+
+print_updates() {
+    if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+        updates_arch=0
+    fi
 
 # if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
 # if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
 if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
-# if ! updates_aur=$(pikaur -Qua 2> /dev/null | wc -l); then
+    # if ! updates_aur=$(pikaur -Qua 2> /dev/null | wc -l); then
     updates_aur=0
 fi
 
 updates=$(("$updates_arch" + "$updates_aur"))
 echo "{\"full_text\": \"$updates\"}"
+}
+case "$1" in
+    --refresh)
+        refresh
+        ;;
+    *)
+        print_updates
+        ;;
+esac
+
--- .zshrc
+++ .zshrc
@@ -59,7 +59,7 @@ alias calc='python -i -c "from math import *"'
 
 alias pacin='trizen -Syu $(trizen -Ssq | fzf)'
 alias pacrm='trizen -Rsnu $(trizen -Qq | fzf)'
-alias pacupgr='trizen -Syu'
+alias pacupgr='trizen -Syu; ~/.config/i3blocks/blocks/updates.sh --update'
 
 # always smart case
 alias rg='rg --smart-case'
