0d903e3f2a12446883110c53f5926e2a7e2f27c0
Nico Ries <nicories@posteo.de>
2019-12-09 15:31:10 +0100

    upd

--- bin/lock.sh
+++ bin/lock.sh
@@ -3,18 +3,18 @@
 file="/tmp/screen.png"
 
 # i3
-maim $file
+# maim $file
 
 # sway
-# grim $file
+grim $file
 
 
-
+# blurring stuff
 convert $file -scale 10% -scale 1000% $file
 [[ -f $1 ]] && convert $file $1 -gravity center -composite -matte $file
 
 # i3
-i3lock -i $file
+# i3lock -i $file
 
 # Sway
-# swaylock -i $file
+swaylock -i $file
